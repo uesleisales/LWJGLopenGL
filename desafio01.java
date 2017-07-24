3f03e74416d910cc900e7a41409146a7cb202bca
Fellipe Meira <fellipe_meira@hotmail.com>
2017-07-24 02:43:33 -0300

    Update desafio01.java

--- desafio01.java
+++ desafio01.java
@@ -1,3 +1,4 @@
+import org.joml.Vector3f;
 import org.lwjgl.BufferUtils;
 import org.lwjgl.opengl.GL;
 import java.nio.ByteBuffer;
@@ -18,6 +19,18 @@ public static float i= 1.f;
 
 public static float testex= 0; 
 public static float testey= 0; 
+public static float auxX= 0; 
+public static int contadorX= 1; 
+public static float auxY= 0; 
+public static int contadorY= 1; 
+public static float replaceX= 0; 
+public static float replaceY= 0; 
+public static float auxX1= 0; 
+
+
+
+
+
     public Desafio01(){
    
         if (!glfwInit()) {
@@ -32,18 +45,18 @@ public static float testey= 0;
         GL.createCapabilities();
         glEnable(GL_TEXTURE_2D);      
                         
-          
+
    
     	
      	float minX = -0.9f;
     	float maxX = 0.9f;
     	Random randx = new Random();
-    	float finalX = randx.nextFloat() * (maxX - minX) + minX;
+    	float quadradoX = randx.nextFloat() * (maxX - minX) + minX;
     	
     	float minY = -1.f;
     	float maxY = 1.f;    	
     	Random randy = new Random();
-    	float finalY = randy.nextFloat() * (maxY - minY) + minY;
+    	float quadradoY = randy.nextFloat() * (maxY - minY) + minY;
     	
     	
     
@@ -62,19 +75,7 @@ public static float testey= 0;
             
             glPushMatrix();
             */
-            glBegin(GL_QUADS);
-            glTexCoord2f(0,0);
-            glVertex2f(finalX, finalY);
             
-            glTexCoord2f(1,0);
-            glVertex2f(finalX + 0.05f, finalY);
-            
-            glTexCoord2f(1,1);
-            glVertex2f(finalX + 0.05f, finalY - 0.05f);
-            
-            glTexCoord2f(0,1);
-            glVertex2f(finalX, finalY - 0.05f);
-            glEnd();
             
             //glPopMatrix();
            
@@ -91,35 +92,71 @@ public static float testey= 0;
             glTranslatef(0.f , 0.f , 1.f);
             */
       
-            if (i<300.f){
+            /*if (i<300.f){
         		i+=0.01f;
         	}     
+            */
             
-            if(testex < finalX){
-            	testex += 0.00001f;
+              Vector3f square = new Vector3f(quadradoX + 0.025f, quadradoY - 0.025f, 0);
+              Vector3f triangle = new Vector3f(0.0f, 0.1f, 0);  
+              double angle = Math.toDegrees(square.angle(triangle));
+              
+              
+            /*if(quadradoX > 0){
+            	auxX = 	quadradoX / 10000; 
+            	
+            	if(contadorX < 10000){            	 
+            	replaceX = auxX * contadorX;
+            	contadorX = contadorX + 1;            	
+              }
             }
+            */
             
-            
-            if(testex > finalX){
-            	testex -= 0.00001f;
+            if(quadradoX < 0 || quadradoX > 0){
+            	auxX = 	quadradoX / 10000; 
+            	
+            	if(contadorX < 10000){            	 
+            	replaceX = auxX * contadorX;
+            	contadorX = contadorX + 1;            	
+              }
             }
+                       
+//////////////////////////
             
-            if(testey < finalY){
-            	testey += 0.00001f;
+           /* if(quadradoY > 0){
+            	auxY = 	quadradoY / 10000; 
+            	
+            	if(contadorY < 10000){            	 
+            	replaceY = auxY * contadorY;
+            	contadorY = contadorY + 1;            	
+              }
             }
-
-            
-            if(testey > finalY){
-            	testey -= 0.00001f;
+        */
+            if(quadradoY < 0 || quadradoY > 0){
+            	auxY = 	quadradoY / 10000; 
+            	
+            	if(contadorY < 10000){            	 
+            	replaceY = auxY * contadorY;
+            	contadorY = contadorY + 1;            	
+              }
             }
+ 
             
-        
 glPushMatrix ( ); // (salva a matrix) para mover o objeto independente do resto da cena 
 
-	//glRotatef ( 10.f+i, 0.f, 0.f, 1.f );
+	/*
+	if(quadradoX > 0){
+	glRotatef ((float)-angle, 0.f, 0.f, 1.f );	
+	}else{
+	glRotatef ((float)angle, 0.f, 0.f, 1.f );	
+	}
+	*/
 
-	glTranslatef (testex, testey, 0.f);
-	glBegin(GL_TRIANGLES);      
+
+	glTranslatef (replaceX+0.025f, replaceY-0.1f, 0.f);
+	
+	glBegin(GL_TRIANGLES);    
+    glColor3f( 1.f, 1.f, 1.f );
 	glVertex2f(-0.1f, 0.f);
 	glVertex2f(0.1f, 0.f);
 	glVertex2f(0.f, 0.2f); 
@@ -132,11 +169,24 @@ glPopMatrix ( ); // (carrega a ultima matrix salva) volta para o resto da cena
            // glPopMatrix();
             
             
-        	//System.out.println(finalX);
-        	//System.out.println(finalY);
+        	//System.out.println(quadradoX);
+        	//System.out.println(quadradoY);
         	
 
-            
+glBegin(GL_QUADS);
+glColor3f( 0.f, 0.f, 1.f );
+glTexCoord2f(0,0);
+glVertex2f(quadradoX, quadradoY);
+
+glTexCoord2f(1,0);
+glVertex2f(quadradoX + 0.05f, quadradoY);
+
+glTexCoord2f(1,1);
+glVertex2f(quadradoX + 0.05f, quadradoY - 0.05f);
+
+glTexCoord2f(0,1);
+glVertex2f(quadradoX, quadradoY - 0.05f);
+glEnd();
             
             glfwSwapBuffers(win);            
                     
